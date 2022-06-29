5b534c726ed7c4a19c8b0b544c576da6d0a33afd
legion15q <sur-pogosyan@mail.ru>
2022-06-30 00:12:38 +0300

    add dockerfiles

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,20 @@
+//main.go
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/test/", DoHealthCheck).Methods("GET", "POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+func DoHealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, i'm a golang microservice")
+	w.WriteHeader(http.StatusAccepted) //RETURN HTTP CODE 202
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module nginx_docker
+module web_app
 
-go 1.17
+go 1.13
+
+require github.com/gorilla/mux v1.8.0
